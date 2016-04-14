6bc6378b15dd26fabfa3bc24a8af75e41a459a64
Kevin McMinn <mcminnk6017@live.com>
2016-04-14 11:57:16 -0400

    UI imprvements

--- src/edu/bsu/kmcminn/CS336/UI.java
+++ src/edu/bsu/kmcminn/CS336/UI.java
@@ -4,12 +4,16 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -25,8 +29,13 @@ public class UI extends Application{
 	
 	private Button searchButton = new Button ("Search");
 	private Button goBackButton = new Button ("Go Back");
-	private TextField searchField = new TextField ();
+	//private TextField searchField = new TextField ();
+	private TextField numberField1 = new TextField();
+	private TextField numberField2 = new TextField();
 	private Text databaseDisplayText = new Text();
+	
+	ComboBox choiceBox = new ComboBox();
+	
 	
 	@Override
 	public void start(Stage primaryStage){
@@ -65,10 +74,22 @@ public class UI extends Application{
 	}
 	
 	public void makeSearchScene(){
+		choiceBox.getItems().addAll("Select pilots with hours between two bounds", "2", "3");
 		Label searchLabel = new Label ("Enter search query here:");
+		
+		HBox numberFields = new HBox(20);
+		numberFields.getChildren().addAll(numberField1, numberField2);
+		numberFields.setAlignment(Pos.CENTER);
+		
+		HBox boundLabels = new HBox(175);
+		Label lowerBoundLabel = new Label ("From:");
+		Label upperBoundLabel = new Label ("To:");
+		boundLabels.getChildren().addAll(lowerBoundLabel, upperBoundLabel);
+		boundLabels.setAlignment(Pos.CENTER);
+		
 		VBox layout1 = new VBox(20);
 		layout1.setAlignment(Pos.CENTER);
-		layout1.getChildren().addAll(searchLabel, searchField, searchButton );
+		layout1.getChildren().addAll(searchLabel, choiceBox,boundLabels, numberFields, searchButton );
 		searchScene = new Scene(layout1, 600, 600);
 	}
 	
@@ -84,8 +105,19 @@ public class UI extends Application{
 	
 	private void searchDatabase() throws SQLException, IOException{
 		JdbcCheckup3 dataBase = new JdbcCheckup3();
-		String results = dataBase.searchDB(searchField.getText());
+		String searchString = createSearchString();
+		String results = dataBase.searchDB(searchString);
 		databaseDisplayText.setText(results);
+	}
+	
+	private String createSearchString(){
+		String searchString = null;
+		if (choiceBox.getValue() == "Select pilots with hours between two bounds"){
+			searchString = "Select * from PILOT, FLOWN_BY WHERE PILOT.Pilot_ID = FLOWN_BY.Pilot_ID AND HOURS BETWEEN " + 
+					numberField1.getText() + " AND " + numberField2.getText() + ";";
+		
+		}
+		return searchString;
 	}
 
 	
