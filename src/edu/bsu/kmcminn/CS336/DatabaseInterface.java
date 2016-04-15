444a6bfbf93a03ef2a4b578c2e902e50aa925946
Braydon <foxden00@gmail.com>
2016-04-15 13:42:52 -0400

    Code cleanup.
    Removed unnecessary comments from DatabaseInterface file.

--- src/edu/bsu/kmcminn/CS336/DatabaseInterface.java
+++ src/edu/bsu/kmcminn/CS336/DatabaseInterface.java
@@ -1,11 +1,5 @@
 package edu.bsu.kmcminn.CS336;
 
-/*
- * Try this progam and make it working first.
- *
- * This sample can be used to check the JDBC installation.
-*/
-
 // You need to import the java.sql package to use JDBC
 import java.sql.*;
 
@@ -14,64 +8,42 @@ import java.io.*;
 
 //import oracle.jdbc.OracleDriver;
 
-class DatabaseInterface
-{
-  public static String searchDB(String searchText)
-       throws SQLException, IOException
-  {
-    // Load the Oracle JDBC driver
-    DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+class DatabaseInterface {
+	public static String searchDB(String searchText) throws SQLException, IOException {
+		DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
 
+		String serverName = "csor12c.dhcp.bsu.edu";
+		String portNumber = "1521";
+		String sid = "or12cdb";
+		String url = "jdbc:oracle:thin:@" + serverName + ":" + portNumber + ":" + sid;
+		String queryText = searchText;
+		String results;
 
-    String serverName = "csor12c.dhcp.bsu.edu";
-    String portNumber = "1521";
-    String sid = "or12cdb";
-    String url ="jdbc:oracle:thin:@" + serverName + ":" + portNumber + ":" + sid;
-    String queryText = searchText;
-    String results;
+		Connection conn = DriverManager.getConnection(url, "kmcminn", "3746");
 
-    Connection conn =
-    	      DriverManager.getConnection (url,
-    	           "kmcminn", "3746");
+		// System.out.println ("connected.");
 
+		// Create a statement
+		Statement stmt = conn.createStatement();
 
+		// Do the SQL
+		ResultSet rset = stmt.executeQuery(
+				"Select * from PILOT, FLOWN_BY WHERE PILOT.Pilot_ID = FLOWN_BY.Pilot_ID AND HOURS BETWEEN 100 AND 500");
+		results = "Table \n";
+		results += "--------------------------------------------------------------------------- \n";
 
-    //System.out.println ("connected.");
-
-    // Create a statement
-    Statement stmt = conn.createStatement ();
-
-    // Do the SQL
-    ResultSet rset = stmt.executeQuery ("Select * from PILOT, FLOWN_BY WHERE PILOT.Pilot_ID = FLOWN_BY.Pilot_ID AND HOURS BETWEEN 100 AND 500");
-    results = "Table \n";
-    results += "--------------------------------------------------------------------------- \n";
-    
-    System.out.println("Table:");
-    System.out.println("---------------------------------------------------------------------------");
-    while (rset.next ()){
-    	results += rset.getString (1) + " " + rset.getString(2)
-      		  + " " + rset.getString(3)
-      		  + " " + rset.getString(4)
-      		  + " " + rset.getString(5)
-      		  + " " + rset.getString(6) +"\n";
-      System.out.println (rset.getString (1) + " " + rset.getString(2)
-    		  + " " + rset.getString(3)
-    		  + " " + rset.getString(4)
-    		  + " " + rset.getString(5)
-    		  + " " + rset.getString(6));
-    }
-
-    //System.out.println ("Your JDBC installation is correct.");
-
-    // close the resultSet
-    rset.close();
-
-    // Close the statement
-    stmt.close();
-
-    // Close the connection
-    conn.close();
-	return results;
-  }
+		System.out.println("Table:");
+		System.out.println("---------------------------------------------------------------------------");
+		while (rset.next()) {
+			results += rset.getString(1) + " " + rset.getString(2) + " " + rset.getString(3) + " " + rset.getString(4)
+					+ " " + rset.getString(5) + " " + rset.getString(6) + "\n";
+			System.out.println(rset.getString(1) + " " + rset.getString(2) + " " + rset.getString(3) + " "
+					+ rset.getString(4) + " " + rset.getString(5) + " " + rset.getString(6));
+		}
+		rset.close();
+		stmt.close();
+		conn.close();
+		return results;
+	}
 
 }
